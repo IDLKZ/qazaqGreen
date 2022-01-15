54d9302e6fe2960023e88fb6300b9a7b063cc8bc
idlkz <idlkz2019@gmail.com>
2022-01-15 14:48:06 +0600

    12345

--- resources/views/material.blade.php
+++ resources/views/material.blade.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 @endpush
 @section('content')
-    <div class="container py-5">
+    <div class="container my-container py-5">
         <div class="under-title">
             <h1>Материалы</h1>
         </div>
