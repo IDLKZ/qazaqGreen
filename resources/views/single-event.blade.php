791ccfba3fcd95fca96a091080ec9792f820adbf
idlkz <idlkz2019@gmail.com>
2022-01-25 22:53:58 +0600

    12345

--- resources/views/single-event.blade.php
+++ resources/views/single-event.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.default')
+@push('css')
+    <style>
+        .card-body img {width: 100%!important;height: auto!important;margin: auto}
+    </style>
+@endpush
 @section('content')
 
     <div class="container my-container py-3">
-        <h3 class="card-title">{{$new->title}}</h3>
-        <div class="card mb-3">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <div class="img-fluid rounded-start" style='background: url("{{$new->getFile('image')}}") no-repeat center;background-size: cover;width: 100%;height: 300px'></div>
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$new->title}}</h5>
-                        <p class="card-text">
-                            {!! $new->description !!}
-                        </p>
-                        <p class="card-text"><small class="text-muted">{{$new->created_at->format('d.m.Y')}}</small></p>
-                    </div>
-                </div>
+        {{--        <h3 class="card-title">{{$new->title}}</h3>--}}
+        <div class="card mb-3 border-0">
+            <div class="img-fluid rounded-start" style='background: url("{{$new->getFile('image')}}") no-repeat center;background-size: contain;width: 100%;height: 450px'></div>
+            <div class="card-body">
+                <h5 class="card-title">{{$new->title}}</h5>
+                <p class="card-text">
+                    {!! $new->description !!}
+                </p>
+                <p class="card-text"><small class="text-muted">{{$new->created_at->format('d.m.Y')}}</small></p>
             </div>
         </div>
     </div>
